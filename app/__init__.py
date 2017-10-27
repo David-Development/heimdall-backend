fcf97796a8cb317077bf0f3ce077f31e9a2f653e
David Luhmer <david-dev@live.de>
2017-10-27 17:26:18 +0200

    Add option to set the redis host/port. Sync database on start. Fix bug that app won't start if no model is present

--- app/__init__.py
+++ app/__init__.py
@@ -24,10 +24,9 @@ app = Flask(__name__)
 api = Api(app)
 AppConfig(app, os.path.join(basedir, 'default_config.py'))
 db = SQLAlchemy(app)
-r = redis.StrictRedis(host='localhost', port='6379', db=1)
+r = redis.StrictRedis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'], db=1)
 
 # turn the flask app into a socketio app
-
 socketio = SocketIO()
 
 recognizer = Recognizer(shape_predictor_path=app.config['DLIB_SHAPE_PREDICTOR_PATH'],
@@ -54,32 +53,38 @@ def extensions(flask_app, main):
     if main:
         # Initialize SocketIO as server and connect it to the message queue.
         socketio.init_app(flask_app,
-                          message_queue='redis://')
+                          message_queue='redis://' + app.config['REDIS_HOST'] + ':' + app.config['REDIS_PORT'])
     else:
         r.flushdb()
         # Initialize SocketIO to emit events through the message queue.
         # Celery does not use eventlet. Therefore, we have to set async_mode
         # explicitly.
         socketio.init_app(None,
-                          message_queue='redis://')
+                          message_queue='redis://' + app.config['REDIS_HOST'] + ':' + app.config['REDIS_PORT'])
     celery.conf.update(flask_app.config)
 
     return None
 
 
 def init_models():
+    # Resync database on startup
+    resources.resync_db()
+
     from tasks import create_classifier, load_classifier
 
     db_model = models.ClassifierStats.query.filter_by(loaded=True).first()
-    app.clf = load_classifier(db_model.model_path)
-    app.labels = db_model.labels_as_dict()
-
+    
     if db_model is None:
         app.clf = create_classifier()
         path = app.config['ML_MODEL_PATH'] + os.sep + '*.pkl'
-        latest_model = max(glob.glob(path), key=os.path.getctime)
-        app.clf = load_classifier(latest_model)
-        db_model = models.ClassifierStats.query.order_by(models.ClassifierStats.date.desc()).first()
+        modelList = glob.glob(path)
+        if modelList:
+            latest_model = max(modelList, key=os.path.getctime)
+            app.clf = load_classifier(latest_model)
+            db_model = models.ClassifierStats.query.order_by(models.ClassifierStats.date.desc()).first()
+            app.labels = db_model.labels_as_dict()
+            db_model.loaded = True
+            db.session.commit()
+    else:
+        app.clf = load_classifier(db_model.model_path)
         app.labels = db_model.labels_as_dict()
-        db_model.loaded = True
-        db.session.commit()
