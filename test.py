95a25ec644a3f3948b091d0a031c0a68d88f3c7c
David Luhmer <david-dev@live.de>
2017-12-11 17:35:03 +0100

    add mqtt handler

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import sys
 import redis
 import concurrent.futures
 import cv2
+from app.mqtt_handler import MqttHandler
+import time
 
 r = redis.StrictRedis(host="redis", port="6379", charset="utf-8", decode_responses=True)
 fred = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -17,38 +19,57 @@ def check_server():
         sys.exit(1)
 
 
-def f(x):
-    res = x * x
-    r.rpush("test", res)
-
-
-
-def main():
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
-    for num in fred:
-        executor.submit(f, num)
-
+def testVideoCamera():
     cap = cv2.VideoCapture('http://192.168.10.48:8080/cam.mjpg')
 
     counter = 0
-    while counter < 100: # read 100 frames
+    while counter < 100:  # read 100 frames
         ret, frame = cap.read()
         print(ret)
-        #cv2.imshow('Video', frame)
-        #if cv2.waitKey(1) == 27:
+        # cv2.imshow('Video', frame)
+        # if cv2.waitKey(1) == 27:
         #    exit(0)
 
         counter += 1
 
     executor.shutdown()
 
-    print(r.lrange("test", 0, -1))
+
+def testThreadPool():
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+    for num in fred:
+        executor.submit(f, num)
+
+def f(x):
+    res = x * x
+    r.rpush("test", res)
+
+
+def testMQTT():
+    mqttHandler = MqttHandler()
+    mqttHandler.connect()
+
+    for i in range(10):
+        time.sleep(1)
+
+    mqttHandler.disconnect()
+
+
+def main():
+
+    # Test MQTT
+    testMQTT()
+
+    #testThreadPool()
+
+    #testVideoCamera()
+
+    # print(r.lrange("test", 0, -1))
 
 
 ####################
 
 if __name__ == "__main__":
-    check_server()
-    ###
-    r.delete("test")
+    #check_server()
+    #r.delete("test")
     main()
