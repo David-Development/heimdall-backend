b7fff036ff880605b5f9a065e2239fb0f8c34f2f
David Luhmer <david-dev@live.de>
2017-12-14 20:41:41 +0100

    Fix bug that a wrong timestamp is used during event creation

--- heimdall/models/Event.py
+++ heimdall/models/Event.py
@@ -12,5 +12,12 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     begindate = db.Column(db.DateTime)
 
-    def __init__(self, begindate=datetime.datetime.now()):
+    # Function default values are evaluated when the function is defined, not when it is called. Your default values
+    # are set in stone the moment Python loads the module.
+    # def __init__(self, begindate=datetime.datetime.now()):  # this does not work!
+
+    def __init__(self, begindate=None):
+        if begindate is None:
+            begindate = datetime.datetime.now()
+
         self.begindate = begindate
