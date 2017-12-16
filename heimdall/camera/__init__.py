f3bd598a0555085abf8804278a54f7cb2402e321
David Luhmer <david-dev@live.de>
2017-12-16 09:03:28 +0100

    Set default "black" picture to 800x600

--- heimdall/camera/__init__.py
+++ heimdall/camera/__init__.py
@@ -1,6 +1,6 @@
 from heimdall.camera.camera import Camera
 from PIL import Image as PILImage
 
-im = PILImage.new("RGB", (640, 480), "black")
+im = PILImage.new("RGB", (800, 600), "black")
 im.save('/live_view.jpg')
 Camera.load_image('/live_view.jpg')
